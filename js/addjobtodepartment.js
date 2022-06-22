db11e066be1c864c266931e03c3f9cbc417b49e3
warat09 <y-title@hotmail.com>
2022-06-23 02:05:22 +0700

    [ADD]Optionjob

--- js/addjobtodepartment.js
+++ js/addjobtodepartment.js
@@ -1,6 +1,8 @@
 var select = document.getElementById("demo");
-var selectjob = document.getElementById("job")
 var form = document.getElementById("form")
+var selectjob = document.getElementById("job")
+selectjob[0]= new Option("pls select department to add job");
+
 
 fetch('http://localhost:9090/Job/AddJobToDepartment')
   .then(res => res.json())
@@ -16,29 +18,41 @@ fetch('http://localhost:9090/Job/AddJobToDepartment')
             select.options[i] = new Option(department[i-1].Department_Name,department[i-1].ID);
         }
   }
-     for(j = 0;j<job.length+1;j++){
-      if(j==0){
-        selectjob.options[j] = new Option("select job");
-      }
-      else{
-        selectjob.options[j] = new Option(job[j-1].JOB,job[j-1].ID);
-      }
-
-    }
-
   } )
 
   form.addEventListener('submit',async(event)=>{
     event.preventDefault()
-    var x = document.getElementById("demo").selectedIndex;
-    var y = document.getElementById("job").selectedIndex;
+    var x = document.getElementById("demo");
+    var y = document.getElementById("job");
 
-    console.log("Department id is ",x)
-    console.log("Job id is ",y)
+    console.log("Department id is ",x.value)
+    console.log("Job id is ",y.value)
+    console.log("Department index is ",x.selectedIndex)
+    console.log("Job index is ",y.selectedIndex)
     if(x !=0 && y !=0){
-      select.remove(x)
-      selectjob.remove(y)
+      selectjob.remove(selectjob.selectedIndex)
     }
 
 })
+async function myFunction () {
+  var x = document.getElementById("demo");
+  selectjob.innerText = null;
+
+
+  console.log(x)
+  if(x.selectedIndex != 0){
+    const response = await fetch('http://localhost:9090/Job/OptionJob?' + new URLSearchParams({
+      IdDepartment: x.value
+    }))
+    const responsedata = await response.json();
+    var job = responsedata.job
+    console.log(job.length)
+    for(j = 0;j<=job.length-1;j++){
+      selectjob[j]= new Option(job[j].JOB,job[j].ID);
+    }
+  }
+  else{
+    selectjob[0]= new Option("pls select");
+  }
+}
 
