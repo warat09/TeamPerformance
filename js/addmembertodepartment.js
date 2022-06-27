88eb605f7cdc5f7b4155d74b7918ab605a91f48a
warat09 <y-title@hotmail.com>
2022-06-27 12:28:28 +0700

    [ADD] page membertodepartment

---
+++ js/addmembertodepartment.js
@@ -0,0 +1,70 @@
+var select = document.getElementById("department");
+var form = document.getElementById("form")
+var selectjob = document.getElementById("member")
+selectjob[0]= new Option("pls select department to add job");
+
+
+fetch('http://localhost:9090/Department/AllDepartment')
+  .then(res => res.json())
+  .then(data =>{
+    // var job = data.job
+    var department = data.Department
+    for (i=0;i<department.length+1;i++){
+        if(i==0){
+            select.options[i] = new Option("selecysepartment");
+        }
+        else{
+            select.options[i] = new Option(department[i-1].Department_Name,department[i-1].ID);
+        }
+    }
+  } )
+
+  form.addEventListener('submit',async(event)=>{
+    event.preventDefault()
+    var x = document.getElementById("demo");
+    var y = document.getElementById("job");
+
+    console.log("Department id is ",x.value)
+    console.log("Job id is ",y.value)
+    console.log("Department index is ",x.selectedIndex)
+    console.log("Job index is ",y.selectedIndex)
+    if(x.selectedIndex !=0 && y.selectedIndex !=0){
+      const response = await fetch('http://localhost:9090/Job/AddJobToDepartment',{
+          method:'post',
+          headers:{
+              'Content-Type':'application/json'    
+          },
+          body: JSON.stringify({
+              "job":y.value,
+              "department":x.value
+          })
+      })
+
+      const responseStatus = await response.json();
+      console.log(responseStatus)
+        selectjob.remove(selectjob.selectedIndex)
+    }
+
+})
+async function myFunction () {
+  var x = document.getElementById("demo");
+  selectjob.innerText = null;
+
+
+  console.log(x)
+  if(x.selectedIndex != 0){
+    const response = await fetch('http://localhost:9090/Job/OptionJob?' + new URLSearchParams({
+      IdDepartment: x.value
+    }))
+    const responsedata = await response.json();
+    var job = responsedata.job
+    console.log(job.length)
+    for(j = 0;j<=job.length-1;j++){
+      selectjob[j]= new Option(job[j].JOB,job[j].ID);
+    }
+  }
+  else{
+    selectjob[0]= new Option("pls select");
+  }
+}
+
