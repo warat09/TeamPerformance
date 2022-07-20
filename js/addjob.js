5b7652dc78b6b084d49099011bbde5c956c5fac9
WARAT YENJAIPRASERT <s6204062616111@kmutnb.ac.th>
2022-07-21 02:02:34 +0700

    [ADD]css

--- js/addjob.js
+++ js/addjob.js
@@ -35,7 +35,18 @@ const main =async()=>{
     console.log(Userdata.menu)
     var checklink = 0
     Userdata.menu.forEach((Item)=> {
+        var setting = document.getElementById("setting");
         if (Item.menuId === 7) {
+            var menu = `
+            <ul>
+                <li><a href="./">Home</a></li>
+                <li><a class="active" href="./AddJob.html">AddJob</a></li>
+                <li><a href="./AddMember.html">AddMember</a></li>
+                <li><a href="./AddJobToDepartment.html">AddJobToDepartment</a></li>
+                <li><a href="./AddMemberToDepartment.html">AddMemberToDepartment</a></li>
+            </ul>
+            `
+            setting.innerHTML = menu;
             checklink = 1
         }
         return checklink
@@ -77,19 +88,45 @@ main()
 
 form.addEventListener("submit",async(event)=>{
     event.preventDefault()
-    var job = document.getElementById("job").value
-    const response = await fetch('http://localhost:9090/Job/AddJob',{
-        method:'post',
-        headers:{   
-            'Content-Type':'application/json'    
-        },
-        body: JSON.stringify({
-            "job":job
-        })
+    Swal.fire({
+        title: 'Do you want to save the changes?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        denyButtonText: `Don't save`,
+      }).then(async(result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          var job = document.getElementById("job").value
+                const response = await fetch('http://localhost:9090/Job/AddJob',{
+                method:'post',
+                headers:{   
+                    'Content-Type':'application/json'    
+                },
+                body: JSON.stringify({
+                    "job":job
+                })
 
-    })
-    const responseStatus = await response.json();
-    console.log(responseStatus)
+            })
+        const responseStatus = await response.json();
+        console.log(responseStatus)
+            if(responseStatus.status ==0){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: `${responseStatus.message}`,
+                    footer: '<a href="">Why do I have this issue?</a>'
+                })
+            }
+            else{
+                Swal.fire(`${responseStatus.message}`, '', 'success')
+            }
+        
+        } else if (result.isDenied) {
+          Swal.fire('Changes are not saved', '', 'info')
+        }
+      })
+    
 })
 const logout =()=>{
     window.localStorage.clear();
--- js/home.js
+++ js/home.js
@@ -2,6 +2,8 @@ var checktoken = localStorage.getItem("tokenlogin")
 var selectchangedepartment = document.getElementById("changedepartment")
 var department = document.getElementById("department")
 
+var members,jobs
+
 const main =async()=>{
     if(checktoken === null || checktoken == " ") {
     window.location.href = './login.html'
@@ -43,15 +45,18 @@ const main =async()=>{
         var setting = document.getElementById("setting");
         if (Item.menuId === 7) {
             var menu = `
-            <p><a href="./AddJob.html">AddJob</a></p>
-            <p><a href="./AddMember.html">AddMember</a></p>
-            <p><a href="./AddJobToDepartment.html">AddJobToDepartment</a></p>
-            <p><a href="./AddMemberToDepartment.html">AddMemberToDepartment</a></p>
+            <ul>
+                <li><a class="active" href="./">Home</a></li>
+                <li><a href="./AddJob.html">AddJob</a></li>
+                <li><a href="./AddMember.html">AddMember</a></li>
+                <li><a href="./AddJobToDepartment.html">AddJobToDepartment</a></li>
+                <li><a href="./AddMemberToDepartment.html">AddMemberToDepartment</a></li>
+            </ul>
             `
             setting.innerHTML = menu;
         }
     });
-    fetch('http://localhost:9090/Member/AllScore?' + new URLSearchParams({
+    var a = await fetch('http://localhost:9090/Member/AllScore?' + new URLSearchParams({
         IdDepartment: selectchangedepartment.options[selectchangedepartment.selectedIndex].value
      }))
     .then(res => res.json())
@@ -62,6 +67,11 @@ const main =async()=>{
         let headerRow = document.createElement('tr');
         let table = document.getElementById('mytable').tBodies[0]
 
+        var newTH = document.createElement('th');
+        newTH.className = "before"
+        newTH.innerHTML = "Name"
+        headerRow.appendChild(newTH)
+        tablerow.appendChild(headerRow);
         job.forEach(headerText => {
             var newTH = document.createElement('th');
             
@@ -92,9 +102,97 @@ const main =async()=>{
     //     a.push(tablerow.rows[0].cells[i].innerHTML)
     // }
     // console.log(a)
-    console.log(member)
-    console.log(job)
+        return [member,job]
     } )
+    var keep=[]
+    var score = a[0]
+    var len = score.length, output = [];
+
+    for(var i = 0; i < len; i++){
+        var scorea =[]
+        console.log(score[i].name)
+        convertedArray = Object.keys(score[i]).map((k,index) => {
+            if(index != 0){
+                console.log(score[i][k])
+                scorea.push(score[i][k])
+            }
+        })
+        console.log(scorea)
+        const randomNum = () => Math.floor(Math.random() * (235 - 52 + 1) + 52);
+        const randomRGB = () => `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+        var r = randomNum()
+        var g = randomNum()
+        var b = randomNum()
+        var c = {
+            label:score[i].name,
+            data:scorea,
+            fill: false,
+            backgroundColor: `rgba(${r}, ${g}, ${b}, 0.2)`,
+            borderColor: `rgb(${r}, ${g}, ${b})`,
+            pointBackgroundColor: `rgb(${r}, ${g}, ${b})`,
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: `rgb(${r}, ${g}, ${b})`
+        }
+
+console.log(randomRGB());
+        keep.push(c)
+        // console.log(convertedArray);
+
+    // output.push(score[i].name)
+    }
+    console.log(keep)
+
+// console.log(output);
+var name = "sdsdsdsdsd"
+var score = [1,2,3]
+    var b = {
+        label:name,
+        data:score
+    }
+    output.push(b)
+    console.log(output)
+    const data = {
+        labels: a[1],
+        datasets: keep
+      };
+    //   [{
+    //     label: 'My First Dataset',
+    //     data: [5, 3, 4],
+    //     fill: false,
+    //     backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    //     borderColor: 'rgb(255, 99, 132)',
+    //     pointBackgroundColor: 'rgb(255, 99, 132)',
+    //     pointBorderColor: '#fff',
+    //     pointHoverBackgroundColor: '#fff',
+    //     pointHoverBorderColor: 'rgb(255, 99, 132)'
+    //   }, {
+    //     label: 'My Second Dataset',
+    //     data: [2, 4, 4],
+    //     fill: false,
+    //     backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    //     borderColor: 'rgb(54, 162, 235)',
+    //     pointBackgroundColor: 'rgb(54, 162, 235)',
+    //     pointBorderColor: '#fff',
+    //     pointHoverBackgroundColor: '#fff',
+    //     pointHoverBorderColor: 'rgb(54, 162, 235)'
+    //   }]
+      
+      const config = {
+        type: 'radar',
+        data: data,
+        options: {
+          elements: {
+            line: {
+              borderWidth: 3
+            }
+          }
+        },
+      };
+        const myChart = new Chart(
+          document.getElementById('myChart'),
+          config
+        );
 
 }
 main()
@@ -118,42 +216,61 @@ const changedepartment=async()=>{
         for (var x=rowCount-1; x>=0; x--) {
             table.deleteRow(x);
         }
-        for (var x=rowName-1; x>0; x--) {
+        for (var x=rowName-1; x>=0; x--) {
         tableall.rows[0].deleteCell(x);
         }
-
-        job.forEach(headerText => {
+        if(job.length != 0){
+            // var tablerow = document.getElementById("mytable").tHead;
+            // let headerRow = document.createElement('tr');
+            // let table = document.getElementById('mytable').tBodies[0]
+    
+            // var newTH = document.createElement('th');
+            // newTH.className = "before"
+            // newTH.innerHTML = "Name"
+            // headerRow.appendChild(newTH)
+            // tablerow.appendChild(headerRow);
             var newTH = document.createElement('th');
+                
+                tablerow.rows[0].appendChild(newTH);
+                newTH.className = "before"
+                newTH.innerHTML = "Name"
+            // headerRow.appendChild(newTH)
+            // tablerow.appendChild(headerRow);
             
-            tablerow.rows[0].appendChild(newTH);
-            newTH.style.width = "150px";
-            newTH.innerHTML = `${headerText}`
-
-        // headerRow.appendChild(header);
+            
+            job.forEach(headerText => {
+                var newTH = document.createElement('th');
+                
+                tablerow.rows[0].appendChild(newTH);
+                newTH.style.width = "150px";
+                newTH.innerHTML = `${headerText}`
+    
+            // headerRow.appendChild(header);
+            // tablerow.appendChild(headerRow);
+    
+        });
         // tablerow.appendChild(headerRow);
-
-    });
-    // tablerow.appendChild(headerRow);
-    member.forEach((emp,i) => {
-        let row = document.createElement('tr');
-        Object.values(emp).forEach((text,i) => {
-            let cell = document.createElement('td');
-                cell.innerHTML = `<td>${text}</td>`
-            row.appendChild(cell);
-            
-        })
-        table.appendChild(row);
-    });
-    // console.log(tablerow.rows[0].cells.length)
-    // console.log(document.getElementById("mytable").tHead.rows[0].cells[1].innerHTML)
-    // var a=[]
-    // for(var i = 1;i < tablerow.rows[0].cells.length;i++){
-    //     console.log(tablerow.rows[0].cells[i].innerHTML)
-    //     a.push(tablerow.rows[0].cells[i].innerHTML)
-    // }
-    // console.log(a)
-    console.log(member)
-    console.log(job)
+        member.forEach((emp,i) => {
+            let row = document.createElement('tr');
+            Object.values(emp).forEach((text,i) => {
+                let cell = document.createElement('td');
+                    cell.innerHTML = `<td>${text}</td>`
+                row.appendChild(cell);
+                
+            })
+            table.appendChild(row);
+        });
+        // console.log(tablerow.rows[0].cells.length)
+        // console.log(document.getElementById("mytable").tHead.rows[0].cells[1].innerHTML)
+        // var a=[]
+        // for(var i = 1;i < tablerow.rows[0].cells.length;i++){
+        //     console.log(tablerow.rows[0].cells[i].innerHTML)
+        //     a.push(tablerow.rows[0].cells[i].innerHTML)
+        // }
+        // console.log(a)
+        console.log(member)
+        console.log(job)
+        }
     } )
 }
 
